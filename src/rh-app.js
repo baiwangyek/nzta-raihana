fab7d18e9b754883f5be57dba77c1963a9b239c3
Bai Wang <yekbaiwang93@gmail.com>
2018-09-23 10:40:02 +1200

    fixed hosting

--- src/rh-app.js
+++ src/rh-app.js
@@ -13,9 +13,6 @@ import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils
 import '@polymer/app-route/app-location.js';
 import '@polymer/app-route/app-route.js';
 import '@polymer/iron-pages/iron-pages.js';
-import '@polymer/iron-selector/iron-selector.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import './my-icons.js';
 import RhButton from './elements/rh-button.js';
 
 // Gesture events like tap and track generated from touch will not be
